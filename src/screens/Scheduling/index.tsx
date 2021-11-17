e87a526cb1bdd3baaf95a0eb40017739137834b1
Riquelme <matheusriquelme10@hotmail.com>
2021-11-17 17:45:35 -0300

    First commit

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { useTheme } from "styled-components";
+
+import { BackButton } from "../../components/BackButton";
+
+import ArrowSVG from '../../assets/arrow.svg'
+import { 
+    Container,
+    Header,
+    Title,
+    RentalPeriod,
+    DateInfo,
+    DateTitle,
+    DateValue,
+    Content,
+    Footer
+} from './styles'
+import { Button } from "../../components/Button";
+import { Calendar } from "../../components/Calendar";
+
+export function Scheduling(){
+    const theme = useTheme()
+    return (
+        <Container>
+            <Header>
+                <StatusBar
+                    barStyle="light-content"
+                    translucent
+                    backgroundColor="transparent"
+                />
+                <BackButton 
+                    onPress={() => {}}
+                    color={theme.colors.shape}
+                />
+
+                <Title>
+                    Escolha uma {'\n'}
+                    data de início e {'\n'}
+                    fim do aluguel
+                </Title>
+
+                <RentalPeriod>
+                    <DateInfo>
+                        <DateTitle>DE</DateTitle>
+                        <DateValue selected={false}>18/06/2021</DateValue>
+                    </DateInfo>
+
+                    <ArrowSVG />
+
+                    <DateInfo>
+                        <DateTitle>ATE</DateTitle>
+                        <DateValue selected={false}>18/06/2021</DateValue>
+                    </DateInfo>
+                </RentalPeriod>
+            </Header>
+
+            <Content>
+                <Calendar />
+            </Content>
+
+            <Footer>
+                <Button title="Confirmar"/>
+            </Footer>
+        </Container>
+    )
+}
